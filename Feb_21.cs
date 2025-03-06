98c451dea1e38e96fb86dfa1bc1e70d401e02b50
Xy70k <97648568+Xy70k@users.noreply.github.com>
2025-03-06 10:26:29 -0700

    Create Feb_21.cs

---
+++ Feb_21.cs
@@ -0,0 +1,6 @@
+""//, is used to identify a string
+  string firstName = "Best";
+int number = 34;
+double temp = 23;
+Console.WriteLine($"Hello, {firstName}! You have {number} messages in your inbox. Temperature is {temp} celsius.");//:
+// is used to write the message; Hello, Best! You have 34 messages in your inbox. Temperature is 23 celsius.
