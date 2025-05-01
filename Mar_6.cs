1f14782f957a029e8f44ed0ba9a985732280759a
Xy70k <97648568+Xy70k@users.noreply.github.com>
2025-05-01 10:04:58 -0600

    Create Mar_6.cs

---
+++ Mar_6.cs
@@ -0,0 +1,5 @@
+// I learned how to use character literals, integer literals, and floating-point literals
+Console.WriteLine('b');
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(12.39816m);
